bb1d44e7927868d7e755d8e48a17c3406c9f9da4
xenonsb <49775704+xenonsb@users.noreply.github.com>
2020-02-08 16:53:23 -0500

    Selects BG_only div for full coverage

--- jstris-sprint-coach.user.js
+++ jstris-sprint-coach.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jstris Sprint Coach
 // @namespace    http://tampermonkey.net/
-// @version      0.15
+// @version      0.16
 // @description  Changes background colour when you go too slow (based on user-defined PPS thresholds)
 // @author       fourwide (xenonsb), Oki
 // @match        https://*.jstris.jezevec10.com/*
@@ -20,6 +20,10 @@ localStorage.thresholdOrange = localStorage.thresholdOrange || "0";
 
 document.body.style['background-image'] = '';
 
+// green: background = 'linear-gradient(rgba(0,255,0,0.15), rgba(0,255,0,0.15))'
+// orange: background = 'linear-gradient(rgba(255,255,0,0.15), rgba(255,255,0,0.15))'
+// red: background = 'linear-gradient(rgba(255,0,0,0.15), rgba(255,0,0,0.15))'
+
 var pbOption = document.createElement("table");
 pbOption.innerHTML = `<tbody><tr><td><input name='bgPPS' onclick="localStorage.backgroundOpt=this.checked"id='backgroundPPS'type="checkbox"><label for="backgroundPPS">Change background colour</label></td><td></td></tr><tr><td colspan="2"><span class="settingsDesc">to Red if PPS below <input oninput='localStorage.thresholdRed=this.value' id='tRed' style="width:50px"></span></td></tr><tr><td colspan="2"><span class="settingsDesc">to Orange if PPS below <input oninput='localStorage.thresholdOrange=this.value' id='tOrange' style="width:50px"></span></td></tr></tbody><br>`
 tab_other.appendChild(pbOption);
@@ -33,7 +37,7 @@ if(typeof getParams != "function"){var getParams=a=>{var params=a.slice(a.indexO
 
 function afterRoundStart() {
 	document.body.style['background-image'] = '';
-	document.querySelector('#app').style.background = 'linear-gradient(rgba(0,255,0,0.15), rgba(0,255,0,0.15))';
+	document.querySelector('#BG_only').style.background = 'linear-gradient(rgba(0,255,0,0.15), rgba(0,255,0,0.15))';
 }
 
 var placeBlockFunc = Game['prototype']["startReadyGo"].toString()
@@ -43,13 +47,13 @@ Game['prototype']["startReadyGo"] = new Function(placeBlockFunc);
 function afterPlaceBlock() {
 	if(document.getElementById('backgroundPPS').checked && this['clock']>2 && 0 != this['getPPS']()) {
 		if (this['getPPS']() < +tRed.value) {
-			document.querySelector('#app').style.background = 'linear-gradient(rgba(255,0,0,0.15), rgba(255,0,0,0.15))'
+			document.querySelector('#BG_only').style.background = 'linear-gradient(rgba(255,0,0,0.15), rgba(255,0,0,0.15))'
 		}
 		else if (this['getPPS']() < +tOrange.value) {
-			document.querySelector('#app').style.background = 'linear-gradient(rgba(255,175,0,0.25), rgba(255,175,0,0.25))';
+			document.querySelector('#BG_only').style.background = 'linear-gradient(rgba(255,175,0,0.25), rgba(255,175,0,0.25))';
 		}
 		else if (+tOrange.value <= this['getPPS']()) {
-			document.querySelector('#app').style.background = 'linear-gradient(rgba(0,255,0,0.15), rgba(0,255,0,0.15))';
+			document.querySelector('#BG_only').style.background = 'linear-gradient(rgba(0,255,0,0.15), rgba(0,255,0,0.15))';
 		}
 	};
 };
